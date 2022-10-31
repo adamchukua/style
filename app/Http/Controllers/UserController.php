2ae5f840b04ac3b969294b3b8471c51afa959752
thegradle <adamchuk.v.1@gmail.com>
2022-10-31 10:26:53 +0300

    Add public user profile, link in work

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function show(User $user)
+    {
+        $total_works = count($user->works);
+        $music_works = DB::table('works')
+            ->where('user_id', $user->id)
+            ->where('type', 'music')
+            ->count();
+        $painting_works = DB::table('works')
+            ->where('user_id', $user->id)
+            ->where('type', 'painting')
+            ->count();
+        $literature_works = DB::table('works')
+            ->where('user_id', $user->id)
+            ->where('type', 'literature')
+            ->count();
+
+        return view('users.show', compact('total_works', 'music_works',
+            'painting_works', 'literature_works', 'user'));
+    }
+}
