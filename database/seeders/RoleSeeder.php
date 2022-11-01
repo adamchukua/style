8518e1aa04605ffba94bda6dc82e302baa9a75ad
thegradle <adamchuk.v.1@gmail.com>
2022-11-01 11:49:53 +0200

    Lowercase role names

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,8 +15,8 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        Role::create(["name" => "Author"]);
-        Role::create(["name" => "Expert"]);
-        Role::create(["name" => "Admin"]);
+        Role::create(["name" => "author"]);
+        Role::create(["name" => "expert"]);
+        Role::create(["name" => "admin"]);
     }
 }
